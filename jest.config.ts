d57eca9efad76b8895604979772015d63512c5aa
Ahmed-Bhouri <ahmed.bhouri@gmail.com>
2025-10-19 18:42:00 +0100

    Added test libraries

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import nextJest from 'next/jest'
+import type { Config } from 'jest'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/app/**/layout.tsx',
+    '!src/app/**/page.tsx',
+  ],
+}
+
+export default createJestConfig(config)
