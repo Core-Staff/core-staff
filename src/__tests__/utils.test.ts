6270598e2afa2b4a58be35417d21c79df6fc40d6
Ahmed-Bhouri <ahmed.bhouri@gmail.com>
2025-10-26 11:27:54 +0000

    chore(test): cleanup setup and format tests

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,5 +1,5 @@
-describe('Sanity Check', () => {
-  it('should pass', () => {
-    expect(true).toBe(true)
-  })
-})
+describe("Sanity Check", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
